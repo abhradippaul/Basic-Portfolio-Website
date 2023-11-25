681668940b43c6efbe1f921004d30e3165f4193e
Abhradip Paul <111123303+abhradippaul@users.noreply.github.com>
2023-11-25 11:58:35 +0530

    Some changes are applied

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // javascript
 const mobile_menu_logo = document.querySelector("#mobile-menu")
 const nav_links_div = document.querySelector(".nav-links-div")
+const nav_links = document.querySelector(".nav-links-div")
 let toggle_menu = true
 mobile_menu_logo.addEventListener("click",()=>{
     if(toggle_menu) {
@@ -8,6 +9,13 @@ mobile_menu_logo.addEventListener("click",()=>{
         mobile_menu_logo.src = "./assets/close.png"
         toggle_menu = false
     }else{
+        nav_links_div.style.display = "none"
+        mobile_menu_logo.src = "./assets/hamburger.png"
+        toggle_menu = true
+    }
+})
+nav_links.addEventListener("click",()=>{
+    if(!toggle_menu){
         nav_links_div.style.display = "none"
         mobile_menu_logo.src = "./assets/hamburger.png"
         toggle_menu = true
